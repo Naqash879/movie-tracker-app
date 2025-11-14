187c7a323a1fa169f4497496ab9ba89474bbcb2b
Naqash879 <naqashworkbench@gmail.com>
2025-11-14 19:49:51 +0000

    Update Code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Login.js";
@@ -29,7 +6,6 @@ function App() {
     <>
       <Login />
     </>
->>>>>>> 5001c609f16f6a274aa69d9a0f7b38be189b4250
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,13 @@
 function Login() {
-  return <h1>Login file</h1>;
+  return (
+    <div className="h-screen bg-blue-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-700 ">
+        Tailwind v4 Working 🎉
+      </h1>
+      <h1 className="text-4xl font-bold text-blue-700 mx-2 my-2 ml-1 mr-1 mt-2 mb-1 texx">
+        Tailwind v4 Working 🎉
+      </h1>
+    </div>
+  );
 }
 export default Login;
